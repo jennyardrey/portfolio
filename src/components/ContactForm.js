eb254821f0e82c7257c4e592c6fd5e97097f7a0c
Jenny Ardrey <jenny@ardrey.co.uk>
2020-02-14 19:17:53 +0000

    wrote contact orm component and added state and functionality

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,31 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/contactform.css';
+import Axios from 'axios';
 
 
-const ContactForm = props => (
-	<div>
-	<h1 className="form">Contact me using the form below</h1>
-     <div>
-                    <input type="text" id="name" name="user_name" />
-                </div>
-                <div>
-             
-                    <input type="email" id="mail" name="user_mail" />
-                </div>
-                <div>
-             
-                    <textarea id="msg" name="user_message"></textarea>
-                </div>
-                <div>
-                    <button class="button" id="submit_now" type="submit">Submit!</button>
-                </div>
-                <div>
-                        <button class="button" id="clear_now" type="reset">Clear</button>
+class ContactForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state ={
+message: "",
+name: "",
+email: "",
+buttonText: "Send message",
+sent: false
+		}
+	}
+
+	formSubmit = e => {
+		e.preventDefault()
+		this.setState ({
+			buttonText: "Sending..."
+		})
+		let data = {
+			name: this.state.name,
+			message: this.state.message,
+			email: this.state.email
+		}
+
+		Axios.post('API_URI', data)
+		.then( res => {
+			this.setState({ sent:true }, this.resetForm() )
+		})
+		.catch(() => {
+			this.setState({
+				message: "",
+name: "",
+email: "",
+				buttonText: "Send message",
+			})
+			alert('Your message has not been sent. Please try again')
+		})
+	}
+
+	resetForm = () => {
+		this.setState({
+			message: "",
+name: "",
+email: "",
+buttonText: "Message sent!",
+		})
+	}
+	render() {
+return <form className="contact-form" onSubmit={e => this.formSubmit(e)}>
+		
+		<label className="message" >Type your message Here:</label>
+		<input onChange={e => this.setState({ message: e.target.value })} name="message" className="message-input" type="text" placeholder="Type your message here" value={this.state.message} required></input>
+		
+		<label className="name" >Your name</label>
+<input onChange={e => this.setState({ name: e.target.value })} name="name" className="message-name" type="text" placeholder="Your name" value={this.state.name} required></input>
+
+<label className="email" >Your email</label>
+<input onChange={e => this.setState({ email: e.target.value })} name="email" className="message-email" type="text" placeholder="Your email" value={this.state.email} required></input>
+
+<div className="button-container">
+	<button type="submit" className="button" >{this.state.buttonText}</button>
 </div>
-</div>
-);
+
+	</form>
+	};
+};
 
 
 
 export default ContactForm;
+
