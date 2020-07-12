db7c756a96a9083343a91d0af82a0934a3fd4ea2
Jenny Ardrey <jenny@ardrey.co.uk>
2020-07-12 18:04:16 +0100

    functioning carousel with placeholder images

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import "../styles/Intro.css"
 
 const Intro = () => {
-	return (<div>
+	return (<div className="intro">
 		<h1>Jenny Ardrey</h1>
 		<p>Anim ullamco exercitation culpa amet dolor exercitation ut nisi adipisicing ea quis et consequat ea. Ad culpa excepteur non commodo id adipisicing veniam est. Tempor anim sunt cupidatat id.
 		</p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,82 @@
-import React from 'react';
+/** @jsx jsx */
+import React, { useState } from 'react';
+import { css, jsx } from '@emotion/core'
+import ProjectsContent from './ProjectsContent';
+import ProjectSlide from './ProjectSlide';
+import Arrow from "./Arrow"
+import Dots from './Dots'
+/**
+ *  @function Projects 
+ */
 
-const Projects = () => {
-	return (<div>
-		<ul>
-			<li>one</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>one</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>one</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>one</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
-			<li>Two</li>
+const Projects = props => {
+	const getWidth = () => window.innerWidth
+	console.log(getWidth())
+
+	const [state, setState] = useState({
+		activeIndex: 0,
+		translate: 0,
+		transition: 0.45
+	})
 
 
-		</ul>
-	</div>);
+	const { translate, transition, activeIndex } = state
+
+	const nextSlide = () => {
+		if (activeIndex === props.slides.length - 1) {
+			return setState({
+				...state,
+				translate: 0,
+				activeIndex: 0
+			})
+		}
+		setState({
+			...state,
+			activeIndex: activeIndex + 1,
+			translate: (activeIndex + 1) * getWidth()
+		})
+	}
+
+	const prevSlide = () => {
+		if (activeIndex === 0) {
+			return setState({
+				...state,
+				translate: (props.slides.length - 1) * getWidth(),
+				activeIndex: props.slides.length - 1
+			})
+		}
+
+		setState({
+			...state,
+			activeIndex: activeIndex - 1,
+			translate: (activeIndex - 1) * getWidth()
+		})
+	}
+
+	return (
+		<div css={SliderCSS}>
+			<ProjectsContent
+				translate={translate}
+				transition={transition}
+				width={getWidth() * props.slides.length}
+			>
+				{props.slides.map((slide, i) => (
+					<ProjectSlide key={slide + i} content={slide} />
+				))}
+			</ProjectsContent>
+			<Arrow direction="left" handleClick={prevSlide} />
+			<Arrow direction="right" handleClick={nextSlide} />
+			<Dots slides={props.slides} activeIndex={activeIndex} />
+		</div>
+	)
 }
 
-export default Projects;
+const SliderCSS = css`
+position: relative;
+height: 60vh;
+width: 80vw;
+margin: 0 auto;
+overflow: hidden;
+`
+
+export default Projects
